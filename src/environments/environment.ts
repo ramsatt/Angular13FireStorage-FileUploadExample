b2542f498e4485e6b58230338aebe05611f2b762
Sathish Kumar Ramalingam <ramsatt@gmail.com>
2021-11-12 22:01:39 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB8BBLb1s2oJJ1Kb2_S90Ov84Uha-tgHsg",
+    authDomain: "one-time-ca4e5.firebaseapp.com",
+    projectId: "one-time-ca4e5",
+    storageBucket: "one-time-ca4e5.appspot.com",
+    messagingSenderId: "154217750369",
+    appId: "1:154217750369:web:7a193b769a0616bf82ef20",
+    measurementId: "G-DN8596WVQS"
+  }
+};
